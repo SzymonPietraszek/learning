3da21111f6fc4df54e172b334d4822510f2b0cfb
SzymonPietraszek <SzymonPietraszek1997@gmail.com>
2025-03-24 20:47:08 +0100

    Split golang and python implementations

--- py/trees/main.py
+++ py/trees/main.py
@@ -2,7 +2,7 @@ from bst import BST
 from avl import AVL
 
 if __name__ != "__main__":
-    quit()
+	quit()
 
 # change tree type
 #  -----------------
@@ -18,14 +18,14 @@ tree.delete(0)
 print("Empty tree delete")
 
 for i in [60, 40, 30, 70, 80, 50, 10, 20]:
-    print(f"insert({i})")
-    tree.insert(i)
-    tree.print()
+	print(f"insert({i})")
+	tree.insert(i)
+	tree.print()
 
 for i in [30, 60, 50, 90]:
-    print(f"search({i}): {tree.search(i)}")
+	print(f"search({i}): {tree.search(i)}")
 
 for i in [30, 50, 70, 60]:
-    print(f"delete({i})")
-    tree.delete(i)
-    tree.print()
+	print(f"delete({i})")
+	tree.delete(i)
+	tree.print()
