31c9c554f547b7e7a335abc80672b5d2dc2b6d9e
SzymonPietraszek <SzymonPietraszek1997@gmail.com>
2026-06-30 15:43:06 +0200

    Add preorder non recurential tree traverse

--- py/tree_traversals.py
+++ py/tree_traversals.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class Node:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -20,6 +23,19 @@ def traverse_inorder(n: Node):
     return ret
 
 
+# root left right
+def traverse_preorder(n: Node):
+    q = deque([n])
+    while q:
+        n = q.popleft()
+        print(n.val)
+        if n.left:
+            q.append(n.left)
+        if n.right:
+            q.append(n.right)
+
+
 bst = Node(4, Node(2, Node(1), Node(3)), Node(5, None, Node(6)))
 
-print("traverse inorder", traverse_inorder(bst))
+print("traverse inorder ", traverse_inorder(bst))
+print("traverse preorder", traverse_preorder(bst))
