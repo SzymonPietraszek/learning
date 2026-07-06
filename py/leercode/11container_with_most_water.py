cc75acd82cfba6abc086253fcb25e1523c2e1e8b
SzymonPietraszek <SzymonPietraszek1997@gmail.com>
2026-07-06 16:16:55 +0200

    Add container with most water

---
+++ py/leercode/11container_with_most_water.py
@@ -0,0 +1,46 @@
+# 11. Container With Most Water
+# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+# Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+# Return the maximum amount of water a container can store.
+
+# Notice that you may not slant the container.
+
+
+# Example 1:
+
+
+# Input: height = [1,8,6,2,5,4,8,3,7]
+# Output: 49
+# Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+# Example 2:
+
+# Input: height = [1,1]
+# Output: 1
+
+
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(height) - 1
+        m = 0
+        while l < r:
+            lh, rh = height[l], height[r]
+            h = min(lh, rh)
+            m = max(m, h * (r - l))
+            if lh < rh:
+                while l < r and height[l] <= h:
+                    l += 1
+            else:
+                while l < r and height[r] <= h:
+                    r -= 1
+        return m
+
+
+s = Solution()
+print(s.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+print(s.maxArea([1, 1]))
