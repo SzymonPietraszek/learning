6c5b476b1440b8129d06f72b45810978a727bf2c
SzymonPietraszek <SzymonPietraszek1997@gmail.com>
2026-07-20 09:47:48 +0200

    Add letter combinations of a phone number

--- py/leercode/14longest_common_prefix.py
+++ py/leercode/14longest_common_prefix.py
@@ -1,3 +1,27 @@
+# 14. Longest Common Prefix
+# Write a function to find the longest common prefix string amongst an array of strings.
+
+# If there is no common prefix, return an empty string "".
+
+
+# Example 1:
+
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+# Example 2:
+
+# Input: strs = ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+
+
+# Constraints:
+
+# 1 <= strs.length <= 200
+# 0 <= strs[i].length <= 200
+# strs[i] consists of only lowercase English letters if it is non-empty.
+
+
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
         if not strs:
@@ -14,3 +38,8 @@ class Solution:
                 c = c[:-1]
 
         return c
+
+
+s = Solution()
+print(s.longestCommonPrefix(["flower", "flow", "flight"]))
+print(s.longestCommonPrefix(["dog", "racecar", "car"]))
