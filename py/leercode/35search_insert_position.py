2404dfb554ea4e9b3bdde261e4ada27539ec534c
SzymonPietraszek <SzymonPietraszek1997@gmail.com>
2026-07-24 11:37:45 +0200

    Add search insert position

---
+++ py/leercode/35search_insert_position.py
@@ -0,0 +1,54 @@
+# 35. Search Insert Position
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+
+# Example 1:
+
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+# Example 2:
+
+# Input: nums = [1,3,5,6], target = 2
+# Output: 1
+# Example 3:
+
+# Input: nums = [1,3,5,6], target = 7
+# Output: 4
+
+
+# Constraints:
+
+# 1 <= nums.length <= 104
+# -104 <= nums[i] <= 104
+# nums contains distinct values sorted in ascending order.
+# -104 <= target <= 104
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 1, len(nums) - 1
+        if target <= nums[0]:
+            return 0
+        if nums[r] < target:
+            return len(nums)
+
+        while l <= r:
+            m = (l + r) // 2
+            if target == nums[m]:
+                return m
+            elif target < nums[m]:
+                r = m - 1
+            else:
+                l = m + 1
+
+        return l
+
+
+s = Solution()
+
+print(s.searchInsert([1, 3, 5, 6], 5))
+print(s.searchInsert([1, 3, 5, 6], 2))
+print(s.searchInsert([1, 3, 5, 6], 7))
+print(s.searchInsert([1, 2, 4, 6, 7], 3))
